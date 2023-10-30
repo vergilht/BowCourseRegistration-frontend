ff4221a82532fafcbe18fb93d23c159bf923b6c0
jiwonjeon <j.jeon339@mybvc.ca>
2023-10-30 15:19:42 -0600

    Fix error

--- src/components/pages/AdminForm.js
+++ src/components/pages/AdminForm.js
@@ -1,38 +1,27 @@
+import { AdminNavigation } from "./Navigation";
+
 export const AdminForm = () => {
   return (
     <>
       <AdminNavigation />
       Admin Form
-    </>
       <div>
-            <h1>Admin Navigation</h1>
+        <h1>Admin Navigation</h1>
 
-            <h2>Admin Form</h2>
+        <h2>Admin Form</h2>
 
-            <p>First Name</p>
-            <input type="text" 
-            id="fname"></input>
+        <p>First Name</p>
+        <input type="text" id="fname"></input>
 
-            <p>Last Name</p>
-            <input type="text" 
-            id="lname"></input>
+        <p>Last Name</p>
+        <input type="text" id="lname"></input>
 
-            <p>Phone Number</p>
-            <input type="text" 
-            id="phone"></input>
+        <p>Phone Number</p>
+        <input type="text" id="phone"></input>
 
-            <p>Email</p>
-            <input type="text" 
-            id="email"></input>
-
-
-        </div>
-
-        /*
-        <>
-          <AdminNavigation />
-          Admin Form
-        </>
-        */
+        <p>Email</p>
+        <input type="text" id="email"></input>
+      </div>
+    </>
   );
 };
