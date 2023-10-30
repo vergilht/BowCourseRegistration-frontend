e3fb55b67b1853a3cedbb94e57528aca14eabfad
Ngatus204 <124854113+Ngatus204@users.noreply.github.com>
2023-10-30 09:37:28 -0600

    Update AdminForm.js

--- src/components/pages/AdminForm.js
+++ src/components/pages/AdminForm.js
@@ -1,10 +1,38 @@
-import { AdminNavigation } from "./Navigation";
-
 export const AdminForm = () => {
   return (
     <>
       <AdminNavigation />
       Admin Form
     </>
+      <div>
+            <h1>Admin Navigation</h1>
+
+            <h2>Admin Form</h2>
+
+            <p>First Name</p>
+            <input type="text" 
+            id="fname"></input>
+
+            <p>Last Name</p>
+            <input type="text" 
+            id="lname"></input>
+
+            <p>Phone Number</p>
+            <input type="text" 
+            id="phone"></input>
+
+            <p>Email</p>
+            <input type="text" 
+            id="email"></input>
+
+
+        </div>
+
+        /*
+        <>
+          <AdminNavigation />
+          Admin Form
+        </>
+        */
   );
 };
